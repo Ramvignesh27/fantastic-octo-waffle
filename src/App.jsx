b257a325e561fe327a97db6878b14f39732d5654
Amaresh  S M <amareshsm13@gmail.com>
2025-05-03 14:07:01 +0530

    signed commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          singed commit message
         </p>
       </div>
       <p className="read-the-docs">
